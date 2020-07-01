9ee441ae02771ccec10d16aabb80c399f69bf11f
Abhishek S Patil <iamPatil92@gmail.com>
2020-07-01 18:19:42 +0530

    added #wordBreak

--- Trie.py
+++ Trie.py
@@ -58,7 +58,6 @@ class Trie:
                 self.printAll(root.children[i],temp+[self.idxToChar(i)])
         
     def findAllWithPrefix(self,data):
-        print()
         
         curr = self.root
         temp = []
@@ -72,25 +71,136 @@ class Trie:
             temp.append(x)
             
             curr = curr.children[idx]
+            
         
         self.printAll(curr,temp)
             
+    def count(self):
+        curr = self.root
+        
+        return self.countWords(curr)
+        
+    def countWords(self,root):
+        res=0
+        
+        if root.isEnd:
+            res+=1 
             
+        for i in range(26):
+            if root.children[i]:
+                res += self.countWords(root.children[i])
+                
+        return res
+        
+    def countChildrens(self,root,idx):
+        res=0
+            
+        for i in range(26):
+            if root.children[i]:
+                res += 1
+                idx[0] = i 
+                
+        return res
+        
+    def commonPrefix(self):
+        
+        curr = self.root
+        temp=[]
+        
+        idx=[-1]
+        
+        while self.countChildrens(curr,idx) == 1 and not curr.isEnd:
+            temp.append(self.idxToChar(idx[0]))
+            
+            curr = curr.children[idx[0]]
+            
+        return "".join(temp)
+        
+        
+    def wordsWithgivenLetters(self,arr):
+        hs=[False]*26
+        
+        for x in arr:
+            hs[self.charToIdx(x)] = True
+            
+        #print(hs)
+        
+        curr = self.root
+        res=[]
+        
+        for i in range(26):
+            if hs[i] and curr.children[i]:
+                self.printWordsWithLetters(curr,hs,res,[])
+                
+            #print(i,curr.children[i])
+            
+        return res
+    
+    def printWordsWithLetters(self,root,hs,res,temp):
+        if root.isEnd:
+            res.append("".join(temp))
+            
+        for i in range(26):
+            if hs[i] and root.children[i]:
+                self.printWordsWithLetters(root.children[i],hs,res,temp + [self.idxToChar(i)])
+                
+        
+    def wordBreak(self,word):
+        
+        res = []
+        self.wordBreakAll(word,res,[])
+        
+        return res 
+        
+    def wordBreakAll(self,word,res,temp):
+        
+        if len(word)==0:
+            res.append(temp)
+            print(temp)
+            return
+                
+                
+        for i in range(len(word)):
+            prefix = word[:i+1]
+            
+            print(prefix)
+            if self.search(prefix):
+                
+                self.wordBreakAll(word[i+1:],res,temp+[prefix])
+    
+    
+    
+            
+            
+t = Trie()
 
-trie = Trie()
-
-l = ["abcd","abc","cde","def","ab","abcef"]
+l = ["abcd","abc","ab","abcef","abc"]
 
 for x in l:
     t.insert(x)
     
 for x in l:
-    print(t.search(x))    // all True
+    print(t.search(x))
     
-print(t.search("abd"))     // False
- 
-t.findAll()         //  ["abcd","abc","cde","def","ab","abcef"]
+print(t.search(""))
 
-t.findAllWithPrefix("ab")  //  ["ab","abc","abcd","abcef"]
+#t.findAll()
+
+t.findAllWithPrefix("ab")
                 
-            
+print(t.count())
+
+print(t.commonPrefix())
+
+arr = ['a','b','c','d']
+
+print(t.wordsWithgivenLetters(arr))
+
+t1 = Trie()
+
+l = ["abcd","abc","d","ab","cd","a","bcd","def"]
+
+for x in l:
+    t1.insert(x)
+    
+print(t1.wordBreak("abcd"))
